d14e78f264ac654ddf982715ddae80c00a21b909
alecs396 <a.swainston16@gmail.com>
2022-03-22 21:28:04 -0600

    Created Sidebar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from locale import currency
 import streamlit as st
 import pandas as pd
 import base64
@@ -13,6 +14,12 @@ st.title('Top 100 Crypto Price Change')
 # Page Layout
 col1 = st.sidebar
 col2, col3 = st.columns((2,1))
+
+# Sidebar + Main Panel config
+col1.header('Input Options')
+
+## Sidebar - Currency price unit
+currency_price_unit = col1.selectbox('Select currency for proce', ('USD', 'USDT', 'BTC'))
 
 
 
